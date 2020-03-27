ebad78028346d550ffce573c23a266184c234c04
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-27 22:52:31 +0530

    Update MyMain.java
    import packages

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,11 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
 }
