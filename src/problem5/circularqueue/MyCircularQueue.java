7b71eee1f3baeb101d2a08c30edff00a63158bb6
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-27 22:29:32 +0530

    Update MyCircularQueue.java
    making constructor and getter setter for the fields of the circular queue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -9,4 +9,19 @@ import problem5.node.Node;
 import problem5.student.Student;
 //to implement circular queue
 public class MyCircularQueue {
+   private static Node rear;
+    private static int size;
+
+    public Node getRear() {
+        return rear;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public MyCircularQueue() {
+        rear = null;
+        size = 0;
+    }
+
 }
