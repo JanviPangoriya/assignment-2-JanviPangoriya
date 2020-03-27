d3382c341d3a9fed9e33b130cea02c2c1ddbafcb
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-27 20:54:51 +0530

    Update MyMain.java
    making object of scanner class to take input from the user

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -14,4 +14,5 @@ import java.util.Scanner;
 public class MyMain {
   public static void main(String[] args) {
   MyBinarySearchTree tree = new MyBinarySearchTree();
+    Scanner scanner = new Scanner(System.in);
 }
