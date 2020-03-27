12eb7fc24dbc86670be115df561f72783d25781b
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-27 23:29:30 +0530

    Update MyMain.java
    task completed

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -11,4 +11,16 @@ import problem4.myqueue.MyQueue;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
-}
+   public static void main(String[] args) {
+   MyBinarySearchTree tree = new MyBinarySearchTree();
+   MyQueue queue = new MyQueue();
+   System.out.println("how many elemnt do you want to add");
+     Scanner scanner = new Scanner(System.in);
+     for(int i=0;i<Integer.parseInt(scanner.nextLine();i++)
+         {
+           tree.add(Integer.parseInt(scanner.nextLine()));
+         }
+      queue.enqueue(tree.root);
+      queue.printQueue();
+         }
+         }
