6cb9790a4b7ff0eed8f39a637e30eab2ce355552
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-27 20:31:21 +0530

    Update MyBinarySearchTree.java
    add fields of MyBinarySearchTree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -8,4 +8,7 @@ package problem1.mybst;
 import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+  private TreeNode root;
+  private int countRight;
+  
 }
