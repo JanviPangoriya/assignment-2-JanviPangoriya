692031a90487b168e5834de8fc73292e564291b5
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 10:52:14 +0530

    Update Node.java
    making getter and setter for the node class field

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -13,4 +13,30 @@ public class Node {
         node=treeNode;
         next=null;
     }
+     public TreeNode getNode() {
+
+        return node;
+
+    }
+
+
+    public void setNode(TreeNode node) {
+
+        this.node = node;
+
+    }
+
+
+    public Node getNext() {
+
+        return next;
+
+    }
+
+
+    public void setNext(Node next) {
+
+        this.next = next;
+
+    }
 }
