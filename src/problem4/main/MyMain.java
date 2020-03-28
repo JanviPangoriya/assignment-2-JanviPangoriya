1e79edba853b5d1101f011ca36c07052b098335b
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 11:13:04 +0530

    Update MyMain.java
    add treeNode

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,20 +7,12 @@
 package problem4.main;
 import problem1.mybst.MyBinarySearchTree;
 import problem4.myqueue.MyQueue;
+import problem1.node.TreeNode;
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
    public static void main(String[] args) {
-   MyBinarySearchTree tree = new MyBinarySearchTree();
-   MyQueue queue = new MyQueue();
-   System.out.println("how many elemnt do you want to add");
-     Scanner scanner = new Scanner(System.in);
-     for(int i=0;i<Integer.parseInt(scanner.nextLine();i++)
-         {
-           tree.add(Integer.parseInt(scanner.nextLine()));
+  
          }
-      queue.enqueue(tree.root);
-      queue.printQueue();
-         }
-         }
+}
