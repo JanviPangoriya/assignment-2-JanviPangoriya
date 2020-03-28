8fe201732a0e7f7f6eb36389e922b9fe89ca52cc
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 11:13:53 +0530

    Update MyMain.java
    making object of each class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -13,6 +13,10 @@ import problem1.node.TreeNode;
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        MyQueue q = new MyQueue();
+        TreeNode root = new TreeNode(10);
   
          }
 }
