4099c7bc31f96f2ecfbaa57b3fe1710e727f0645
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 10:50:25 +0530

    Update Node.java
    make two parameterized constructor which is differing in argument

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -1,27 +1,16 @@
 package problem4.myqueue;
 import problem1.node.TreeNode;
 public class Node {
-    private int data;
     private Node next;
+    private TreeNode node;
 
     public Node(int data) {
-        this.data = data;
-        next = null;
+        node=new TreeNode(data);
+        next=null;
     }
-
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
+    public Node(TreeNode treeNode)
+    {
+        node=treeNode;
+        next=null;
     }
 }
