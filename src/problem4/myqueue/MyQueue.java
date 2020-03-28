007ec1859e15dc9ac93f6c1dab6b1e5c256d5bf7
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 10:53:38 +0530

    Update MyQueue.java
    clear the code and rewrite the code

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -10,40 +10,5 @@ import problem1.node.TreeNode;
 import static problem1.mybst.MyBinarySearchTree.preOrder;
 import static problem1.mybst.MyBinarySearchTree.preOrderString;
 public class MyQueue {
-    private Node rear;
-    private Node front;
-    private int size;
-
-    public MyQueue() {
-        front = null;
-        rear = null;
-        size = 0;
-    }
-    public void enqueue(TreeNode root) {
-        preOrder(root);
-        String[] data = preOrderString.toString().split(" ");
-        for (int i = 0; i < data.length; i++) {
-            Node node = new Node(Integer.parseInt(data[i]));
-            if (front == null) {
-                rear = node;
-                front = node;
-                size++;
-            } else {
-                rear.setNext(node);
-                rear = node;
-                size++;
-            }
-        }
-    }
- public void printQueue() {
-        Node temp = this.front.getNext();
-        for (int i = 0; i < this.size - 1; i++) {
-            if (i != this.size - 2) {
-                System.out.print(temp.getData() + "--->");
-                temp = temp.getNext();
-            } else {
-                System.out.println(temp.getData());
-            }
-        }
-    }
+   
 }
