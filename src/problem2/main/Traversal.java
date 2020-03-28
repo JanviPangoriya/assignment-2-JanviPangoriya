4d4880002f1105c950cb6bc38f5c43a9520705d1
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 10:35:54 +0530

    Update Traversal.java
    add a method to print the tree in post order manner

--- src/problem2/main/Traversal.java
+++ src/problem2/main/Traversal.java
@@ -1,4 +1,20 @@
 package problem2.main;
 
 import problem1.node.TreeNode;
-public class Traversal 
+public class Traversal  {
+   public void printPostorder(TreeNode node) {
+
+        if (node == null)
+
+            return;
+
+        printPostorder(node.getLeft());
+
+        printPostorder(node.getRight());
+
+        System.out.print(node.getData() + " ");
+
+    }
+  
+  
+}
