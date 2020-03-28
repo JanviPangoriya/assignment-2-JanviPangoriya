99d94d05072ab276d923b6a6fd6f5aa95da7b855
Janvi Pangoriya <53748515+JanviPangoriya@users.noreply.github.com>
2020-03-28 11:14:36 +0530

    Update MyMain.java
    insert element into the tree

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -17,6 +17,27 @@ public class MyMain {
         MyBinarySearchTree m = new MyBinarySearchTree();
         MyQueue q = new MyQueue();
         TreeNode root = new TreeNode(10);
+      m.insert(root, 5);
+
+        m.insert(root, 1);
+
+        m.insert(root, 15);
+
+        m.insert(root, 9);
+
+        m.insert(root, 7);
+
+        m.insert(root, 12);
+
+        m.insert(root, 30);
+
+        m.insert(root, 25);
+
+        m.insert(root, 40);
+
+        m.insert(root, 45);
+
+        m.insert(root, 6);
   
          }
 }
